57eaeee657c450d1212e0771f76acf8b11b3c10f
ipmach <andre.gg96@gmail.com>
2022-09-17 11:30:14 +0200

    Update data_loader.py

--- src/data_retriever/data_loader.py
+++ src/data_retriever/data_loader.py
@@ -8,7 +8,7 @@ import requests
 # Auxiliar functions
 
 unwanted_things = ["style", "script", 'head', "title", "meta", "[document]"]
-
+STATUS_OK = 200
 
 def remove_unwanted(element):
     # Function from:
@@ -18,6 +18,23 @@ def remove_unwanted(element):
     if isinstance(element, Comment):
         return False
     return True
+
+
+def dict2string(string, dictionary):
+
+    if not isinstance(dictionary, dict) and not isinstance(dictionary, list):
+        return f"{string}\n. {str(dictionary)}" 
+
+    if isinstance(dictionary, dict):
+        for k in dictionary.keys():
+            if k != "link":
+                string = dict2string(string, dictionary[k])
+
+    if isinstance(dictionary, list):
+        for k in dictionary:
+            string = dict2string(string, k)
+
+    return string
 
 #  Data structure
 
@@ -73,7 +90,9 @@ class XMLHandler(IHandler):
     def __init__(self, text, url, **kwargs):
 
         self.url = url
-        self.texts = json.dumps(xmltodict.parse(text))
+        self.texts = xmltodict.parse(text)
+        self.texts = dict2string("", self.texts)
+
         self.date = str(datetime.now())
 
 
@@ -81,16 +100,17 @@ class XMLHandler(IHandler):
 
 def donwload_html(url, handler=BeautyHandler, **kwargs):
     respond = requests.get(url)
-    if respond.status_code == 200:
+    if respond.status_code == STATUS_OK:
         return handler(respond.text, url, **kwargs)
     raise Exception(f"Status respond {respond.status_code}")
 
 
-def donwload_xml(url, handler=XMLHandler):
+def donwload_xml(url, handler=XMLHandler, **kwargs):
     respond = requests.get(url)
-    if respond.status_code == 200:
+    if respond.status_code == STATUS_OK:
         return handler(respond.text, url, **kwargs)
     raise Exception(f"Status respond {respond.status_code}")
 
 
-print(donwload_xml("https://www.dwd.de/DWD/warnungen/cap-feed/de/atom.xml")().json())
+print(donwload_xml("https://www.aemet.es/documentos_d/eltiempo/prediccion/avisos/rss/CAP_AFAE_wah_RSS.xml")().text)
+#print(donwload_xml("https://www.dwd.de/DWD/warnungen/cap-feed/de/atom.xml")().text)
