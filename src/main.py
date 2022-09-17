6978784d40ef7f22e305a9abc223569a66d1b06d
ipmach <andre.gg96@gmail.com>
2022-09-17 18:11:59 +0200

    add german translation

--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ from data_analyzer import analyze_website
 #data_chunks = process_webpage("https://feeds.meteoalarm.org/feeds/meteoalarm-legacy-atom-netherlands", "xml")
 #data_chunks = process_webpage("https://en.wikipedia.org/wiki/Newton%27s_laws_of_motion", "html")
 
-
-document = analyze_website("https://caps.weathersa.co.za/Home/RssFeed", "xml")
+url = "https://www.dwd.de/DWD/warnungen/cap-feed/de/atom.xml"
+document = analyze_website(url, "xml", translation="ger_en")
 
 
 print(document.get_json())
